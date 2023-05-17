93dde2bc79b2c0db22fdfaf21b5db255b72e2464
Jerry Sanju Joanes <jerrysanjujoanes000@gmail.com>
2023-05-17 18:24:24 +0530

    "changed the font and some minor desing changes"

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ document.querySelector('#add').onclick= function(){
     }
     else{
         document.querySelector('#task').innerHTML += `
-        <div class="flex flex-row pt-3 pl-2 ">
-          <button class="w-full bg-slate-400 rounded-full pt-3 pl-2 pr-2" id='taskname' >
+        <div class="flex flex-row pt-3 pl-2 pr-2 ">
+          <button class="w-full bg-slate-300 rounded-full pt-3 pl-2 pr-4" id='taskname' font-handwritting text-9xl shadow-2xl hover:bg-white >
              ${document.querySelector('#newtask').value}
           </button>
         </div>`
